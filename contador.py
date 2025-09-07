b25bac2c61971eabda7d910552fd6cf9355eeb91
phat009 <lopezruizjosemanuel83@gmail.com>
2025-09-07 13:01:57 -0600

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,8 @@
+num = input("Ingrese un numero ")
+numeros = "0123456789"
+contador = 0
+
+for nume in num:
+    if nume in numeros: 
+        contador = contador + 1
+print ("La cantidad de dígitos es: ",contador)
