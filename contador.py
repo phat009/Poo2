344b5c23f7374205850fdf3c13896ba189480e6d
phat009 <lopezruizjosemanuel83@gmail.com>
2025-09-07 13:02:56 -0600

    Update contador.py

--- contador.py
+++ contador.py
@@ -1,8 +1,9 @@
-num = input("Ingrese un numero ")
+num = input("Ingrese un numero: ")
 numeros = "0123456789"
 contador = 0
 
 for nume in num:
     if nume in numeros: 
         contador = contador + 1
+
 print ("La cantidad de dígitos es: ",contador)
